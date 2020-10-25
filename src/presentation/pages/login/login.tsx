3114e5da41f1d075928e7f85f9c0e4d3c5f93b4c
Henrique Schmeller <henrique_schmeller@hotmail.com>
2020-10-25 13:55:16 -0300

    feat: ensure Login will not loading spinner and error on start

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Styles from './login-styles.scss'
 import {
   LoginHeader as Header,
@@ -7,31 +7,45 @@ import {
   Footer
 } from '@/presentation/components'
 
+import FormContext from '@/presentation/contexts/form/form-context'
+
+type StateProps = {
+  isLoading: boolean
+  errorMessage: string
+}
+
 const Login: React.FC = () => {
+  const [state] = useState<StateProps>({
+    isLoading: false,
+    errorMessage: ''
+  })
+
   return (
     <div className={Styles.login}>
       <Header/>
-      <form className={Styles.form}>
-        <h2>Login</h2>
-        <Input
-          type="email"
-          name="email"
-          placeholder="Digite seu e-mail"
-        />
-        <Input
-          type="password"
-          name="password"
-          placeholder="Digite sua senha"
-        />
-        <button className={Styles.submit} type="submit">
-          Entrar
-        </button>
+      <FormContext.Provider value={state}>
+        <form className={Styles.form}>
+          <h2>Login</h2>
+          <Input
+            type="email"
+            name="email"
+            placeholder="Digite seu e-mail"
+          />
+          <Input
+            type="password"
+            name="password"
+            placeholder="Digite sua senha"
+          />
+          <button className={Styles.submit} type="submit">
+            Entrar
+          </button>
 
-        <span className={Styles.link}>
-          Não possui um conta? Crie uma agora mesmo!
-        </span>
-        <FormStatus />
-      </form>
+          <span className={Styles.link}>
+            Não possui um conta? Crie uma agora mesmo!
+          </span>
+          <FormStatus />
+        </form>
+      </FormContext.Provider>
       <Footer />
     </div>
   )
