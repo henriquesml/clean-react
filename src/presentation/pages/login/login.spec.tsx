05012e79906385eaeff425842744e71dbeecfc14
Henrique Schmeller <henrique_schmeller@hotmail.com>
2020-10-25 14:46:07 -0300

    refactor: move SUT creation to a factory method

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -1,36 +1,47 @@
 import React from 'react'
-import { render } from '@testing-library/react'
+import { render, RenderResult } from '@testing-library/react'
 import Login from './login'
+
+type SutTypes = {
+  sut: RenderResult
+}
+
+const MakeSut = (): SutTypes => {
+  const sut = render(<Login />)
+  return {
+    sut
+  }
+}
 
 describe('Componente login', () => {
   test('Não deve renderizar o spinner e o erro ao iniciar', () => {
-    const { getByTestId } = render(<Login />)
-    const errorWrap = getByTestId('error-wrap')
+    const { sut } = MakeSut()
+    const errorWrap = sut.getByTestId('error-wrap')
     expect(errorWrap.childElementCount).toBe(0)
   })
   test('Deve estar desabilidato o botão de login ao inciar', () => {
-    const { getByTestId } = render(<Login />)
-    const submitButton = getByTestId('submit') as HTMLButtonElement
+    const { sut } = MakeSut()
+    const submitButton = sut.getByTestId('submit') as HTMLButtonElement
     expect(submitButton.disabled).toBe(true)
   })
   test('O status do input de email deve estar com status obrigatório ao inciar', () => {
-    const { getByTestId } = render(<Login />)
-    const emailStatus = getByTestId('email-status')
+    const { sut } = MakeSut()
+    const emailStatus = sut.getByTestId('email-status')
     expect(emailStatus.title).toBe('Campo obrigatório')
   })
   test('O status do input de email deve ser 🔴 ao inciar', () => {
-    const { getByTestId } = render(<Login />)
-    const emailStatus = getByTestId('email-status')
+    const { sut } = MakeSut()
+    const emailStatus = sut.getByTestId('email-status')
     expect(emailStatus.textContent).toBe('🔴')
   })
   test('O status do input de senha deve estar com status obrigatório ao inciar', () => {
-    const { getByTestId } = render(<Login />)
-    const passwordtatus = getByTestId('password-status')
+    const { sut } = MakeSut()
+    const passwordtatus = sut.getByTestId('password-status')
     expect(passwordtatus.title).toBe('Campo obrigatório')
   })
   test('O status do input de senha deve ser 🔴 ao inciar', () => {
-    const { getByTestId } = render(<Login />)
-    const passwordtatus = getByTestId('password-status')
+    const { sut } = MakeSut()
+    const passwordtatus = sut.getByTestId('password-status')
     expect(passwordtatus.textContent).toBe('🔴')
   })
 })
