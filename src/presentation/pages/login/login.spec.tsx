6a6c003829984a36a3d889f5ce1e36882489e8d8
Henrique Schmeller <henrique_schmeller@hotmail.com>
2020-10-25 14:13:11 -0300

    test: ensure login starts with submit button disabled

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -8,4 +8,9 @@ describe('Componente login', () => {
     const errorWrap = getByTestId('error-wrap')
     expect(errorWrap.childElementCount).toBe(0)
   })
+  test('Deve estar desabilidato o botão de login ao inciar', () => {
+    const { getByTestId } = render(<Login />)
+    const submitButton = getByTestId('submit') as HTMLButtonElement
+    expect(submitButton.disabled).toBe(true)
+  })
 })
