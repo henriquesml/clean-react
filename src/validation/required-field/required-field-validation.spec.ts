02d7c0d77ad5d08ebc2fbd845c107390b63e2064
Henrique Schmeller <henrique_schmeller@hotmail.com>
2020-10-31 14:39:48 -0300

    test: ensure requiredFieldValidation returns falsy if field is not empty

--- src/validation/required-field/required-field-validation.spec.ts
+++ src/validation/required-field/required-field-validation.spec.ts
@@ -1,10 +1,17 @@
 import { RequiredFieldValidation } from '@/validation/required-field/required-field-validation'
 import { RequiredFieldError } from '@/validation/error'
+import fake from 'faker'
 
 describe('RequiredFieldValidation', () => {
   test('Deve retronar um erro se o campo estiver vazio', () => {
     const sut = new RequiredFieldValidation('email')
     const error = sut.validate('')
     expect(error).toEqual(new RequiredFieldError())
+  })
+
+  test('Deve retornar falsy se o campo não estiver vazio', () => {
+    const sut = new RequiredFieldValidation('email')
+    const error = sut.validate(fake.random.word())
+    expect(error).toBeFalsy()
   })
 })
