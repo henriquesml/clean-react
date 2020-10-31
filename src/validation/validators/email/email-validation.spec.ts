824c64aca02c335f4930241744b64a3760817cc7
Henrique Schmeller <henrique_schmeller@hotmail.com>
2020-10-31 16:11:30 -0300

    test: ensure emailValidation returns falsy if email is valid

--- src/validation/validators/email/email-validation.spec.ts
+++ src/validation/validators/email/email-validation.spec.ts
@@ -1,10 +1,17 @@
+import faker from 'faker'
 import { EmailValidation } from './email-validation'
 import { InvalidFieldError } from '@/validation/errors'
 
 describe('EmailValidation', () => {
   test('Deve retronar um erro se o campo de email for inválido', () => {
-    const sut = new EmailValidation('email')
-    const error = sut.validate('')
+    const sut = new EmailValidation(faker.random.word())
+    const error = sut.validate(faker.random.word())
     expect(error).toEqual(new InvalidFieldError(sut.field))
+  })
+
+  test('Deve retronar falsy se o campo de email for válido', () => {
+    const sut = new EmailValidation(faker.random.word())
+    const error = sut.validate(faker.internet.email())
+    expect(error).toBeFalsy()
   })
 })
