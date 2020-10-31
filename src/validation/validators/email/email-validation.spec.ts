0a7603ae9663a10253224ff7a12fba5bb43b9f6b
Henrique Schmeller <henrique_schmeller@hotmail.com>
2020-10-31 16:36:36 -0300

    fix: ensure emailValidation allows empty value

--- src/validation/validators/email/email-validation.spec.ts
+++ src/validation/validators/email/email-validation.spec.ts
@@ -16,4 +16,10 @@ describe('EmailValidation', () => {
     const error = sut.validate(faker.internet.email())
     expect(error).toBeFalsy()
   })
+
+  test('Deve retronar falsy se o campo de email for vazio', () => {
+    const sut = makeSut()
+    const error = sut.validate('')
+    expect(error).toBeFalsy()
+  })
 })
