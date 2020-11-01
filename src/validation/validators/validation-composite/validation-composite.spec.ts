271af0cd55cf2ac26f6e14ab2ed93492a6e94b4e
Henrique Schmeller <henrique_schmeller@hotmail.com>
2020-11-01 13:57:36 -0300

    test: ensure ValidationComposit returns the first error found

--- src/validation/validators/validation-composite/validation-composite.spec.ts
+++ src/validation/validators/validation-composite/validation-composite.spec.ts
@@ -13,4 +13,17 @@ describe('ValidationComposite', () => {
     const error = sut.validate('any_field', 'any_value')
     expect(error).toBe('any_error_message')
   })
+
+  test('Deve retornar o primeiro erro se qualquer um dos validadores falhar', () => {
+    const fieldValidationSpy = new FieldValidationSpy('any_field')
+    const fieldValidationSpy2 = new FieldValidationSpy('any_field')
+    fieldValidationSpy.error = new Error('first_error')
+    fieldValidationSpy2.error = new Error('second_error')
+    const sut = new ValidationComposite([
+      fieldValidationSpy,
+      fieldValidationSpy2
+    ])
+    const error = sut.validate('any_field', 'any_value')
+    expect(error).toBe('first_error')
+  })
 })
