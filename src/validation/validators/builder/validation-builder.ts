3ae5a0c807c55b115552b4a16d0c1fcfa962e94b
henriquesml <henrique_schmeller@hotmail.com>
2020-11-17 21:39:05 -0300

    refactor: export validations protocols in index file

--- src/validation/validators/builder/validation-builder.ts
+++ src/validation/validators/builder/validation-builder.ts
@@ -1,4 +1,4 @@
-import { FieldValidation } from '@/validation/protocols/field-validation'
+import { FieldValidation } from '@/validation/protocols'
 import { RequiredFieldValidation, EmailValidation, MinLengthValidation } from '@/validation/validators'
 
 export class ValidationBuilder {
